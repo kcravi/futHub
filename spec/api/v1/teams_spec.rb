af1559d3dfbbfe5a1ad6a11399ca9a058394e7ff
Ravi KC <ravikc18@gmail.com>
2018-08-08 20:19:21 -0400

    added team model and controller tests

---
+++ spec/api/v1/teams_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::TeamsController, type: :controller do
+
+  describe "GET#index" do
+    let!(:team) { FactoryBot.create(:team)}
+    let!(:team_1) { Team.create(name: "Man United", city: "Cambridge", state: "MA", zipcode: "02140", description: "Red Devils")}
+
+    it 'should return a list of all the teams' do
+      get :index
+      returned_json = JSON.parse(response.body)
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to eq "application/json"
+
+      expect(returned_json.length).to eq 2
+      expect(returned_json[0]["name"]).to eq team.name
+      expect(returned_json[0]["city"]).to eq team.city
+
+      expect(returned_json[1]["city"]).to eq "Cambridge"
+      expect(returned_json[1]["zipcode"]).to eq "02140"
+    end
+  end
+end
