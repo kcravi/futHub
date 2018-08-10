d3cc8a555eca3da99ae53322988dfc92c37d3942
Ravi KC <ravikc18@gmail.com>
2018-08-10 02:15:35 -0400

    added devise authentication and authorization

--- app/javascript/react/containers/TeamFormContainer.js
+++ app/javascript/react/containers/TeamFormContainer.js
@@ -95,6 +95,9 @@ class TeamFormContainer extends React.Component {
          } else {
            let errorMessage = `${response.status} (${response.statusText})`,
              error = new Error(errorMessage)
+             if(response.status == 401){
+               alert("You must be signed in to add a team!!!")
+             }
            throw(error)
          }
        })
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#admin?" do
+    it "is not an admin if the role is not admin" do
+      user = FactoryBot.create(:user, role: "member", admin: false)
+      expect(user.admin?).to eq(false)
+    end
+
+    it "is an admin if the role is admin and admin is true" do
+      user = FactoryBot.create(:user, role: "admin", admin: true)
+      expect(user.admin?).to eq(true)
+    end
+  end
 end
