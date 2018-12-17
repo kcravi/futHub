c119254dceaf7443a4e1f3aa6e37be6616597aee
Ravi KC <ravikc18@gmail.com>
2018-12-16 23:27:48 -0500

    config photo upload

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
       region: ENV["AWS_REGION"]
     }
     # config.fog_directory  = ENV["S3_BUCKET"]
-    # config.fog_public = false
+    config.fog_public = true
 
     if Rails.env.production?
       config.fog_directory  = ENV["S3_BUCKET_PROD"]
